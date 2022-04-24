e4e817391133b41a996af04a2608590427b035d5
ArtemyZim <art.zim228@yandex.ru>
2022-04-24 21:47:06 +0700

    Cached_server.class fixed

--- src/com/company/naumenTest/Cached_server.java
+++ src/com/company/naumenTest/Cached_server.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Cached_server {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         int serverRequestCount = 0;         // Счетчик обращений к распределенной системе
         int cachedNum;                      // Максимальное число запросов, которое может быть закэшировано
@@ -14,15 +14,11 @@ public class Cached_server {
         String pathOutput = "src/com/company/naumenTest/output.txt";
 
         List <Long> requestsList = new LinkedList<>();      //Создается связный список, который содержит номера запросов
-        Map <Long, Integer> hashKeyCount = new HashMap<>(); //Создается хэшмапа, которая содержит пары "номер запроса, количество"
+        Map <Long, Integer> hashKeysCount = new HashMap<>(); //Создается хэшмапа, которая содержит пары "номер запроса, количество"
         Map <Long, Integer> cachedRequest = new HashMap<>();     //Создается хэшмапа, в которой будут храниться кэшированные запросы
 
-        BufferedReader rd;
-        PrintWriter out;
-
-        try {
-            rd = new BufferedReader(new InputStreamReader(new FileInputStream(pathInput)));
-            out = new PrintWriter(pathOutput);
+        try(BufferedReader rd = new BufferedReader(new FileReader(pathInput));
+            PrintWriter out = new PrintWriter(pathOutput)) {
 
             String [] nums = rd.readLine().split(" ");  /* Считываем первую строку в исходном файле и присваиваем
                                                                 нашим переменным соответствующие значения */
@@ -37,40 +33,38 @@ public class Cached_server {
 
                 // Добавление запросов в хэшмапу.
                 // Каждый запрос добавляется как ключ, а значение в данном ключе - количество таких запросов в файле
-                if (hashKeyCount.get(requestNum) == null) hashKeyCount.put(requestNum, 1);
-                else hashKeyCount.replace(requestNum, hashKeyCount.get(requestNum) + 1);
+                if (hashKeysCount.get(requestNum) == null) hashKeysCount.put(requestNum, 1);
+                else hashKeysCount.replace(requestNum, hashKeysCount.get(requestNum) + 1);
             }
-            rd.close();
 
             long currentNum;                            // Переменная, содержащая текущий запрос, извлекаемый из списка
             for (int i = 0; i < requestsCount; i++){
+
                 currentNum = requestsList.get(i);
+                hashKeysCount.replace(currentNum, hashKeysCount.get(currentNum) - 1);   // Уменьшаем значение текущего ключа на 1
 
                 if (!cachedRequest.containsKey(currentNum)){  // Если в кэше нет текущего запроса (ключа)
 
                     if (cachedRequest.size() < cachedNum){    // Если кэш не "забит" данными
-                        cachedRequest.put(currentNum, hashKeyCount.get(currentNum));   // Добавление текущего запроса в кэш
+                        cachedRequest.put(currentNum, hashKeysCount.get(currentNum));   // Добавление текущего запроса в кэш
                     }
                     else {
                          // Получение ключа из кэша с минимальным значением
                          long key = Collections.min(cachedRequest.entrySet(), Map.Entry.comparingByValue()).getKey();
 
-                         if (cachedRequest.get(key) < hashKeyCount.get(currentNum)){  // Если значение ключа текущего запроса больше
+                         if (cachedRequest.get(key) < hashKeysCount.get(currentNum)){  // Если значение ключа текущего запроса больше
                              cachedRequest.remove(key);                               // Заменяем ключ с наименьшим значением на текущий
-                             cachedRequest.put(currentNum, hashKeyCount.get(currentNum) - 1);
+                             cachedRequest.put(currentNum, hashKeysCount.get(currentNum));
                          }
                     }
                     serverRequestCount++;
-                }
-                hashKeyCount.replace(currentNum, hashKeyCount.get(currentNum) - 1);   // Уменьшаем значение текущего ключа на 1
-            }
 
+                } else cachedRequest.replace(currentNum, hashKeysCount.get(currentNum));
+            }
             out.print(serverRequestCount);
-            out.flush();
-            out.close();
 
         } catch (IOException e){
-            System.out.println("Ошибка! " + e);
+            System.out.println("Ошибка! " + e.getMessage());
         }
     }
 }
